f0cc156406135802599f675e722a254665294ed3
Seung Y. Lee <seung.youn.lee@gmail.com>
2022-02-23 19:42:47 +0900

    commit on 'master'

---
+++ src/pages/invoices.jsx
@@ -0,0 +1,31 @@
+import { Link, Outlet } from "react-router-dom";
+import { getInvoices } from "../data/sales_data";
+
+function Invoices() {
+  let invoices = getInvoices();
+  return (
+    <div style={{ display: "flex" }}>
+      <nav 
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem"
+        }}
+      >
+        {invoices.map(invoice => {
+          return (
+            <Link
+              style={{ display: "block", margin: "1rem 0" }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </Link>
+          );
+        })}
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Invoices;
